49e820e2c008c6902aed1f21d7a9a484d366b6e7
Svedtiva <rileydkennedy2003@gmail.com>
2023-11-16 08:29:52 -0500

    Added temp redirect for family and test home

--- resources/views/Homwefind/login.blade.php
+++ resources/views/Homwefind/login.blade.php
@@ -73,19 +73,30 @@ footer {
     </header>
 
     <main>
-        <div class="login">
-            <label for="username"><b>username</b></label>
-            <input type="text" placeholder="Enter Username" name="username" required>
+        <div class="login" >
+            <form action="/home" method="GET">
+            <label for="ID"><b>Patient ID</b></label>
+            <input type="text" placeholder="Enter Patient ID" name="ID" required>
 
             <label for="password"><b>password</b></label>
-            <input type="text" placeholder="Enter password" name="password" required>
+            <input type="text" placeholder="Enter Patient password" name="password" required>
 
-            <button type="submit">Login</button>
+            <input type="submit" value="Submit">
             <label>
-                <input type="checkbox" checked="checked" name="remember"> Remember me
+            <input type="checkbox" checked="checked" name="remember"> Remember me
             </label>
+            </form>
         </div>
-
+        <div class="Familylogin" >
+            <form action="/familyhome" method="GET">
+            <label for="ID"><b>Or enter Your Family Code Here!</b></label><br>
+            <input type="text" placeholder="Enter Family Code" name="ID" required>
+            <input type="submit" value="Submit">
+            <label>
+            <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+            </form>
+        </div>
     <div class="container" style="background-color:#f1f1f1">
         <button type="button" class="cancelbtn">Cancel</button>
         <span class="psw">Forgot <a href="#">password?</a></span>
