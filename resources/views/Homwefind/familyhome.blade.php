2a4d5d3f1223ad60ffa16eb23488134bbb179724
jaz <jazlynntroop@gmail.com>
2023-11-16 11:24:11 -0500

    html layout

---
+++ resources/views/Homwefind/familyhome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.layout')
+
+
+@section('content')
+    <h2>Welcome to the Family Home Page!</h2>
+    <p>This is the home page content.</p>
+@endsection
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basic HTML Setup</title>
-</head>
-<body>
+@extends('layout.layout')
+
+
+@section('content')
 
     <main>
+
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"><div style='text-align:center; margin-top:30px;'>
             <div style='display: flex; justify-content: center; align-items: center; height: 100vh;'>
             <div style='border: 2px solid #3498db; padding: 20px;'>
@@ -20,8 +17,5 @@
         </div>
     </main>
 
-    <footer>
-    </footer>
-</body>
-</html>
+@endsection
 
