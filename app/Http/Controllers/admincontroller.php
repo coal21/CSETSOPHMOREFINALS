4ffea658aa0585290a73eaff7f58a58ecabe23d8
coal21 <deadcoal21@gmail.com>
2023-11-17 08:07:32 -0500

    I fight motieze

--- app/Http/Controllers/admincontroller.php
+++ app/Http/Controllers/admincontroller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class doctorappointment extends Controller
+class admincontroller extends Controller
 {
     //
 }
---
+++ resources/views/Homwefind/landing.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic HTML Setup</title>
+</head>
+<body>
+
+    <main>
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"><div style='text-align:center; margin-top:30px;'>
+            <div style='display: flex; justify-content: center; align-items: center; height: 100vh;'>
+            <div style='border: 2px solid #3498db; padding: 20px;'>
+            <button style='margin-right:20px; padding:15px 30px; font-size:20px;' ><a href='/login'>Login</a></button>
+            </div>
+            <div style='border: 2px solid #3498db; padding: 20px;'>
+                <button style='padding:15px 30px; font-size:20px;'><a href='/signup'>Sign Up</a></button>
+            </div>
+        </div>
+        </div>
+    </main>
+
+    <footer>
+    </footer>
+</body>
+</html>
+
