b00af72b5b646b8c5eab9381a967af1514c2f76b
coal21 <deadcoal21@gmail.com>
2023-11-21 09:30:01 -0500

    TK sucks

--- app/Http/Controllers/admincontroller.php
+++ app/Http/Controllers/admincontroller.php
@@ -8,12 +8,7 @@ class admincontroller extends Controller
 {
     public function show()
     {
-<<<<<<< HEAD
-        $data=Roles::all();
-        return view('/admin', compact('data'));
-=======
         $roles = Roles::all();
         return view('Homwefind.admin', ['roles' => $roles]);
->>>>>>> bcf9854e59c553766458e45124cc9f77df5bcd74
     }
 }
