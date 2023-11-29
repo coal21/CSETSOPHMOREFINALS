085c72e25a4e89e1f5c4abadf4d628c94f7a3864
Dexter <hannahsolmorgan@gmail.com>
2023-11-29 10:31:36 -0500

    FIXEDDDDDDD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\adminController;
+use App\Http\Controllers\admincontroller;
 use App\Http\Controllers\landingcontroller;
 use App\Http\Controllers\registrationapprovalcontroller;
 use App\Http\Controllers\signupcontroller;
@@ -26,29 +26,42 @@ route::get('/home', [landingcontroller::class,'index']);
 
 
 // Login Routes
-route::get('/login', [loginController::class,'index']);
+route::get('/login', [logincontroller::class,'index']);
 
 Route::post('/login', [LoginController::class, 'login'])->name('loginsubmit');
 
-Route::get('/admin-home', [LoginController::class, 'adminHome']);
+Route::get('/admin-home', [LoginController::class, 'adminHome'])->name('admin.home');
+
+Route::view('/admin-home', 'Homepages.adminhome')->name('admin.home');
 
 Route::get('/supervisor-home', [LoginController::class, 'supervisorHome'])->name('supervisor.home');
 
+Route::view('/supervisor-home', 'Homepages.supervisorhome')->name('supervisor.home');
+
 Route::get('/doctor-home', [LoginController::class, 'doctorHome'])->name('doctor.home');
+
+Route::view('/doctor-home', 'Homepages.doctorhome')->name('doctor.home');
 
 Route::get('/caregiver-home', [LoginController::class, 'caregiverHome'])->name('caregiver.home');
 
+Route::view('/caregiver-home', 'Homepages.caregiverhome')->name('caregiver.home');
+
 Route::get('/patient-home', [LoginController::class, 'patientHome'])->name('patient.home');
+
+Route::view('/patient-home', 'Homepages.patienthome')->name('patient.home');
 
 Route::get('/family-home', [LoginController::class, 'familyHome'])->name('family.home');
 
-Route::get('/approval', [admincontroller::class,'approval']);
+Route::view('/family-home', 'Homepages.patienthome')->name('family.home');
 
-Route::post('/approve_account/{id}', [admincontroller::class, 'approve_account']);
+Route::post("/approve", [admincontroller::class,"approveAccount"]);
+
+Route::get("/awaiting", [admincontroller::class,"awaiting"]);
+
 
 // signup routes
 route::get('/signup', [signupcontroller::class,'index']);
 
-Route::post('/signup/submit', [SignupController::class, 'submit']);
+Route::post('/signup/submit', [signupController::class, 'submit']);
 
 route::redirect('/pending-approval', 'Homwefind.pending_approval');
