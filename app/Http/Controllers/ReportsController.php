7a58e224dae935205b97773b2fc53d93ac6e0bc0
Dexter <hannahsolmorgan@gmail.com>
2023-12-12 10:54:05 -0500

    DID ALOT OF STUFF

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -1,14 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Reports;
 use Illuminate\Http\Request;
-
 
 class ReportsController extends Controller
 {
    public function update(Request $request) {
+
+      $caregiver_id = $request->input("caregiver_id");
+
+      $mm = $request->input("mm");
+      $am = $request->input("am");
+      $nm = $request->input("nm");
+
+      $breakfeast = $request->input("breakfeast");
+      $lunch = $request->input("lunch");
+      $dinner = $request->input("dinner");
 
       
 
